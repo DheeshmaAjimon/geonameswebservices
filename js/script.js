79116ecb799c2f3195f95214337b0223eb69170e
Your Name <you@example.com>
2021-02-03 21:59:58 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,103 @@
+ $( "#search1" ).click(function() {
+
+    $.ajax({
+        url: "php/oceanInfo.php",
+        type: 'POST',
+        dataType: 'json',
+        
+        data: {
+            lat: $('#latitude1').val(),
+            lng: $('#longitude1').val()
+            
+        },
+        success: function(result) {
+
+            console.log(result);
+
+            if(result.status.name == "ok")
+            {
+            
+              $('#oceanName').html(result ['data']['name']);
+                
+            }
+        
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+
+            alert("An error occured:" + jqXHR.status + " " + jqXHR.textStatus);
+            
+        }
+    });
+}); 
+
+   
+
+$( "#search2" ).click(function() {
+
+    $.ajax({
+        url: "php/countryInfo.php",
+        type: 'POST',
+        dataType: 'json',
+        
+        data: {
+            lat: $('#latitude2').val(),
+            lng: $('#longitude2').val()
+            
+        },
+        success: function(result) {
+
+            console.log(result);
+
+            if(result.status.name == "ok")
+            {
+            
+              $('#countryCode').html(result ['data']);
+                
+            }
+        
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+
+            alert("An error occured:" + jqXHR.status + " " + jqXHR.textStatus);
+            
+        }
+    }); 
+
+
+});
+
+$( "#search3" ).click(function() {
+
+    $.ajax({
+        url: "php/addressInfo.php",
+        type: 'POST',
+        dataType: 'json',
+        
+        data: {
+            lat: $('#latitude3').val(),
+            lng: $('#longitude3').val()
+            
+        },
+        success: function(result) {
+
+            console.log(result);
+
+            if(result.status.name == "ok")
+            {
+            
+              $('#houseNumber').html(result ['data']['houseNumber']);
+              $('#locality').html(result ['data']['locality']);
+              $('#street').html(result ['data']['street']);
+               
+            }
+        
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+
+            alert("An error occured:" + jqXHR.status + " " + jqXHR.textStatus);
+            
+        }
+    }); 
+
+
+});
---
+++ php/addressInfo.php
@@ -0,0 +1,32 @@
+<?php
+
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL);  //Enable all PHP errors and warnings
+
+	$executionStartTime = microtime(true);
+
+	
+	$url='http://api.geonames.org/addressJSON?formatted=true&lat=' . $_REQUEST['lat'] . '&lng=' . $_REQUEST['lng'] . '&username=dheeshmaaji&style=full';
+
+	$ch = curl_init();   //create a new cURL resource
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);  //false to stop cURL from verifying the peers certificate
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);   //true to return the transfer as a string of the return value of curl_exec() instead of outputting it directly
+	curl_setopt($ch, CURLOPT_URL,$url);  // URL to fetch
+
+	$result=curl_exec($ch);
+
+	curl_close($ch);
+
+	$decode = json_decode($result,true);  //decodes json to PHP object
+
+	$output['status']['code'] = "200";
+	$output['status']['name'] = "ok";
+	$output['status']['description'] = "mission saved";
+	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime);
+	$output['data'] = $decode['address'];
+	
+	header('Content-Type: application/json; charset=UTF-8');
+
+	echo json_encode($output); 
+
+?>
